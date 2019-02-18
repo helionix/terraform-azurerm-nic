35da199184a1a2e8e1573567899c7ed4d13404b9
Oleksii Borodai <oleksii.borodai@digitallyinspired.com>
2019-02-18 18:07:32 +0200

    fix typo

--- main.tf
+++ main.tf
@@ -30,7 +30,6 @@ resource "azurerm_network_interface" "this" {
     private_ip_address_allocation  = "${var.private_ip_address_allocation}"
     subnet_id                      = "${var.subnet_id}"
     private_ip_address             = "${var.private_ip_address}"
-    application_security_group_ids = "${var.application_security_group_ids}"
   }
 
   tags = "${var.tags}"
